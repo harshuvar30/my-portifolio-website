48eeded7c4eb46a49ccca38c2305b69593cb06d7
Harshvardhan <hv9796923@gmail.com>
2025-12-15 01:03:25 +0530

    fixed build issue

--- src/components/SkillHeading.tsx
+++ src/components/SkillHeading.tsx
@@ -1,7 +1,7 @@
 function SkillHeading({title} : {title: string}){
     return (
        <h3 className="text-2xl font-semibold tracking-tight leading-none ">
-                  EarthRenewal AI Farmer
+                  EarthRenewal AI Farmer {title}
         </h3>
     )
 }
